2c5925b50415ec53de455a745f6fc6802870c0b6
kimtang2 <kimtang1006@gmail.com>
2026-08-08 23:38:43 +0800

    Add 奶糖视频 subscription source
    Add new subscription source for 奶糖视频.

--- index.config.js
+++ index.config.js
@@ -73,6 +73,7 @@ var index_config_default = {
  t4: {list: [
         { name: "聚合传媒", address: "https://php.doube.eu.org/" },
         { name: "5lxtv", address: "https://raw.githubusercontent.com/kimtang2/Adult-/refs/heads/main/51xtv.js" }
+{ name: "奶糖视频", address: "https://cdn.jsdelivr.net/gh/kimtang2/Adult-/naitang.json" }
    
         // 以后新增的订阅源，继续在这里添加，用逗号分隔
     ]
