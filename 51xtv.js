7b7b3a407da5146b8c0aac01c49bb529277fd27b
kimtang2 <kimtang1006@gmail.com>
2026-08-09 00:56:09 +0800

    Update JSON structure in 51xtv.js

--- 51xtv.js
+++ 51xtv.js
@@ -1,169 +1,228 @@
 {
-  "tag": "短裙",
-  "list": [
-    {
-      "vod_id": "snis-946",
-      "vod_name": "毫无防备地诱惑内衣的小恶魔JK桥本Arina",
-      "vod_pic": "https://v-proxy.imgcaches.cc/images/av/9eef516a8c196867b51a639311ed5428.jpg",
-      "vod_actor": "未知女优",
-      "vod_director": "whos.tv",
-      "vod_remarks": "HD高清",
-      "vod_pubdate": "2017-06-24",
-      "vod_area": "日本",
-      "vod_year": "2017",
-      "vod_tags": [
-        "中文字幕",
-        "女高中生",
-        "美丽的女孩",
-        "短裙",
-        "单体作品",
-        "恶作剧",
-        "超薄格",
-        "高清",
-        "独家",
-        "女子校生",
-        "パンチラ",
-        "単体作品",
-        "イタズラ",
-        "ギリモザ",
-        "ハイビジョン",
-        "独占配信"
-      ],
-      "vod_content": "<p>正在查看影片【snis-946】的百科，本页面提供橋本ありな (新ありな)主演作品《毫无防备地诱惑内衣的小恶魔JK桥本Arina》的高清精彩画面截图、作品资料、精彩画面、高清在线播放功能。</p><p style=\"margin-top:8px;color:#666;font-size:12px;\"><b>标签：</b>中文字幕 | 女高中生 | 美丽的女孩 | 短裙 | 单体作品 | 恶作剧 | 超薄格 | 高清 | 独家 | 女子校生 | パンチラ | 単体作品 | イタズラ | ギリモザ | ハイビジョン | 独占配信</p>",
-      "vod_play_from": "whos.tv",
-      "vod_play_url": "https://v-proxy.imgcaches.cc/9eef516a8c196867b51a639311ed5428/9eef516a8c196867b51a639311ed5428.m3u8",
-      "type_name": "成人影片",
-      "actress_avatar": ""
-    },
-    {
-      "vod_id": "mida-368",
-      "vod_name": "我是个处女宅男，被儿时玩伴的无套内裤照撩起性欲。我是个宅男，被保险套盒撩起性欲，用光了所有保险套。宫下玲奈",
-      "vod_pic": "https://v-proxy.imgcaches.cc/images/av/c29eef2a94a19432bbc7e55848305426.jpg",
-      "vod_actor": "未知女优",
-      "vod_director": "whos.tv",
-      "vod_remarks": "HD高清",
-      "vod_pubdate": "2025-10-20",
-      "vod_area": "日本",
-      "vod_year": "2025",
-      "vod_tags": [
-        "美丽的女孩",
-        "辣妹",
-        "青梅竹马",
-        "苗条",
-        "短裙",
-        "单体作品",
-        "高清",
-        "独家",
-        "スレンダー",
-        "単体作品",
-        "幼なじみ",
-        "ギャル",
-        "パンチラ",
-        "独占配信",
-        "ハイビジョン"
-      ],
-      "vod_content": "<p>正在查看影片【mida-368】的百科，本页面提供宮下玲奈主演作品《我是个处女宅男，被儿时玩伴的无套内裤照撩起性欲。我是个宅男，被保险套盒撩起性欲，用光了所有保险套。宫下玲奈》的高清精彩画面截图、作品资料、精彩画面、高清在线播放功能。</p><p style=\"margin-top:8px;color:#666;font-size:12px;\"><b>标签：</b>美丽的女孩 | 辣妹 | 青梅竹马 | 苗条 | 短裙 | 单体作品 | 高清 | 独家 | スレンダー | 単体作品 | 幼なじみ | ギャル | パンチラ | 独占配信 | ハイビジョン</p>",
-      "vod_play_from": "whos.tv",
-      "vod_play_url": "https://v-proxy.imgcaches.cc/c29eef2a94a19432bbc7e55848305426/c29eef2a94a19432bbc7e55848305426.m3u8",
-      "type_name": "成人影片",
-      "actress_avatar": ""
-    },
-    {
-      "vod_id": "urvrsp-252",
-      "vod_name": "[VR][8K VR]最近搬到我管理的公寓的绫濑先生是一个笨拙的女孩和喜怒无常的下流绫濑心",
-      "vod_pic": "https://v-proxy.imgcaches.cc/images/av/8f2a12395c37d187b2eefda765874b69.jpg",
-      "vod_actor": "未知女优",
-      "vod_director": "whos.tv",
-      "vod_remarks": "HD高清",
-      "vod_pubdate": "2023-08-18",
-      "vod_area": "日本",
-      "vod_year": "2023",
-      "vod_tags": [
-        "女大学生",
-        "大乳房",
-        "短裙",
-        "单体作品",
-        "中出",
-        "独家",
-        "虚拟现实",
-        "高品质 VR",
-        "8KVR",
-        "中出し",
-        "女子大生",
-        "パンチラ",
-        "独占配信",
-        "VR専用",
-        "単体作品",
-        "ハイクオリティVR"
-      ],
-      "vod_content": "<p>正在查看影片【urvrsp-252】的百科，本页面提供綾瀬こころ主演作品《[VR][8K VR]最近搬到我管理的公寓的绫濑先生是一个笨拙的女孩和喜怒无常的下流绫濑心》的高清精彩画面截图、作品资料、精彩画面、高清在线播放功能。</p><p style=\"margin-top:8px;color:#666;font-size:12px;\"><b>标签：</b>女大学生 | 大乳房 | 短裙 | 单体作品 | 中出 | 独家 | 虚拟现实 | 高品质 VR | 8KVR | 中出し | 女子大生 | パンチラ | 独占配信 | VR専用 | 単体作品 | ハイクオリティVR</p>",
-      "vod_play_from": "whos.tv",
-      "vod_play_url": "https://v-proxy.imgcaches.cc/8f2a12395c37d187b2eefda765874b69/8f2a12395c37d187b2eefda765874b69.m3u8",
-      "type_name": "成人影片",
-      "actress_avatar": ""
-    },
-    {
-      "vod_id": "cmv-148",
-      "vod_name": "胯绳 DID 衣服束缚女人被绳子绑起来痛苦扭动 2",
-      "vod_pic": "https://v-proxy.imgcaches.cc/images/av/6490b5389d9bef39f920221d0bb17914.jpg",
-      "vod_actor": "未知女优",
-      "vod_director": "whos.tv",
-      "vod_remarks": "HD高清",
-      "vod_pubdate": "2020-11-07",
-      "vod_area": "日本",
-      "vod_year": "2020",
-      "vod_tags": [
-        "约束",
-        "SM",
-        "短裙",
-        "捆绑",
-        "广告偶像",
-        "独家",
-        "パンチラ",
-        "縛り・緊縛",
-        "イメージビデオ",
-        "独占配信"
-      ],
-      "vod_content": "<p>正在查看影片【cmv-148】的百科，本页面提供初島うい主演作品《胯绳 DID 衣服束缚女人被绳子绑起来痛苦扭动 2》的高清精彩画面截图、作品资料、精彩画面、高清在线播放功能。</p><p style=\"margin-top:8px;color:#666;font-size:12px;\"><b>标签：</b>约束 | SM | 短裙 | 捆绑 | 广告偶像 | 独家 | パンチラ | 縛り・緊縛 | イメージビデオ | 独占配信</p>",
-      "vod_play_from": "whos.tv",
-      "vod_play_url": "https://v-proxy.imgcaches.cc/6490b5389d9bef39f920221d0bb17914/6490b5389d9bef39f920221d0bb17914.m3u8",
-      "type_name": "成人影片",
-      "actress_avatar": ""
-    },
-    {
-      "vod_id": "mida-245-uncensored-leak",
-      "vod_name": "MIDA-245 “看到我的内裤，你们就硬起来了吗？”她用紧身内裤诱惑老师们。她在她们耳边低语，然后给她们打飞机，让她们的鸡巴硬起来，所以就让我爽一爽吧……柚木悠悠 - 夕月ゆる",
-      "vod_pic": "https://v-proxy.imgcaches.cc/images/av/2f37e61dec8d27be18582989fe72c535.jpg",
-      "vod_actor": "未知女优",
-      "vod_director": "whos.tv",
-      "vod_remarks": "HD高清",
-      "vod_pubdate": "2025-07-11",
-      "vod_area": "日本",
-      "vod_year": "2025",
-      "vod_tags": [
-        "无码流出",
-        "制服",
-        "美丽的女孩",
-        "短裙",
-        "单体作品",
-        "手淫",
-        "淫语",
-        "高清",
-        "独家",
-        "無修正リーク",
-        "パンチラ",
-        "単体作品",
-        "手コキ",
-        "脏话",
-        "ハイビジョン",
-        "独占配信"
-      ],
-      "vod_content": "<p>正在查看影片【mida-245-uncensored-leak】的百科，本页面提供素人主演作品《MIDA-245 “看到我的内裤，你们就硬起来了吗？”她用紧身内裤诱惑老师们。她在她们耳边低语，然后给她们打飞机，让她们的鸡巴硬起来，所以就让我爽一爽吧……柚木悠悠 - 夕月ゆる》的高清精彩画面截图、作品资料、精彩画面、高清在线播放功能。</p><p style=\"margin-top:8px;color:#666;font-size:12px;\"><b>标签：</b>无码流出 | 制服 | 美丽的女孩 | 短裙 | 单体作品 | 手淫 | 淫语 | 高清 | 独家 | 無修正リーク | パンチラ | 単体作品 | 手コキ | 脏话 | ハイビジョン | 独占配信</p>",
-      "vod_play_from": "whos.tv",
-      "vod_play_url": "https://v-proxy.imgcaches.cc/2f37e61dec8d27be18582989fe72c535/2f37e61dec8d27be18582989fe72c535.m3u8",
-      "type_name": "成人影片",
-      "actress_avatar": ""
-    }
-  ],
-  "count": 5
+  "code": 0,
+  "msg": "success",
+  "data": {
+    "page": 1,
+    "pagecount": 1,
+    "limit": 24,
+    "total": 36,
+    "list": [
+      {
+        "vod_id": "2871",
+        "vod_name": "JRZE-226-C 初撮り人妻ドキュメント",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/94d70c0e2a173beac7a867772cac4f9c.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2870",
+        "vod_name": "JRZE-222-C 初撮り人妻ドキュメント",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/2fc8beb6c908614eb9be470217147410.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2869",
+        "vod_name": "JRZE-223-C 初撮り人妻ドキュメント",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/292c53abc9b88787e4f070b20e652549.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2868",
+        "vod_name": "JRZE-224-C 初撮り人妻ドキュメント",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/b8b6a85872cddd2587c2c9559c6115cc.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2835",
+        "vod_name": "MVSD-602-C 乙女の体液にまみれながら何度も射精する真夏のびちゃびちゃドロドロ舐めじゃくり汁だく性交",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260223-1/99e2ee9da610d6336b4df84fa8d938fb.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2834",
+        "vod_name": "MVSD-606-C 教え子の性の悩みを解消してくれるノーブラノーパン美波先生のビンビン身の下相談",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260223-1/accd494b50781bacdfe9f636e4f37356.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2832",
+        "vod_name": "MVSD-605-C そこのアナタ！今日からワタシの性処理当番に任命します！（拒否権ナシ） スクールカーストTO",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260223-1/8303bf5faccf51c0c7c84b6ec50da1ba.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2831",
+        "vod_name": "MVSD-604-C 眩しいほどの美貌 溢れ出る品格 狂い咲く変態性 肛門中毒症のデカ尻マゾ女医がケツ穴で狂った",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260223-1/db158494deaf61232f3969ccaa73bfbf.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2807",
+        "vod_name": "MVSD-578-C 鬼畜デカマラ親父の媚薬キメセクにアクメ漬けにされてしまった美人家庭教師 絶叫！痙攣！汗だく",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/90ca88170836bcb88907392a5400416d.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2805",
+        "vod_name": "MVSD-573-C 真夏のMアクメ強化特訓 パワハラ先輩達に来る日も来る日も嬲られ犯され輪姦されてマゾの悦びを",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/4e8039c605c416190b34b59e5e8c13ea.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2804",
+        "vod_name": "MVSD-581-C 父さん…新しい義母さん、めちゃめちゃ美人で可愛いけどなんだか色々ヤバいです…。 親父が不在",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/831268d2e20c163ca8bbe549683e62ff.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2803",
+        "vod_name": "MVSD-579-C 会員限定！美人セラピストがぬるぬる生マ〇コと淫らな腰使いで射精に導いてくれるマンコキ回春エステ",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/0f7deccd9c2780e7690f179a3478a9eb.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2867",
+        "vod_name": "JRZE-233-C 初撮り人妻ドキュメント",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/7d475ccef9fc4025e897dc2c43759490.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2866",
+        "vod_name": "JRZE-235-C 初撮り五十路妻ドキュメント",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/bf5a6a03dbbe968ea93607154395f3a9.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2865",
+        "vod_name": "JRZE-232-C 初撮り人妻ドキュメント",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/d68a3f127dd957e98185c345eb2840aa.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2864",
+        "vod_name": "JRZE-236-C 初撮り人妻ドキュメント",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/e281e825832f667b9b44f15ba323768d.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2863",
+        "vod_name": "JRZE-234-C 初撮り人妻ドキュメント",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/155dffd1fb66c94a9d9753d71f432535.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2830",
+        "vod_name": "MVSD-607-C ごめんなさい…おもらしエステ 禁欲で疼くぼっち乙女の肉体をジワジワ狂わせる失禁オイルマッサージ",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260223-1/a32d96294e1baaba590b07846cd3d503.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2824",
+        "vod_name": "MVSD-588-C 制服美少女派遣サービス エンジェルスイート。 制服マニアおやじと従順美少女が体液まみれで絡",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/fbf0c7c0d28160c34ac1bce93be407cb.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2823",
+        "vod_name": "MVSD-589-C マザコンおやじに来る日も来る日も乳首をもてあそばれ開発され乳首イキを覚えてしまったワタシ。",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/869f83dc4853d2f19e162054102b0211.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2802",
+        "vod_name": "MVSD-582-C 彼女に飲ませるはずの媚薬を間違って彼女の姉が飲んでしまい… 媚薬効果12時間！ 絶倫×絶倫",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/1ceb9197d1cbb37a02fc7def699ec521.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2801",
+        "vod_name": "MVSD-583-C Wブルジョワ人妻の華麗なるマラ遊び 若い男のチ〇ポエキスを吸い尽くすマンイーター熟女のフェ",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/91485ca0bee57f50263cc1bd335b9432.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2800",
+        "vod_name": "MVSD-587-C どうせなら…最後に1発ヤラせてくれ！！！ 容姿端麗で成績優秀な無気力美少女に童貞キモオタ絶",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/609140f806173eb55219eb4d597a478c.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2785",
+        "vod_name": "MVSD-547-C 無差別おしゃぶりオチ◯ポコレクション",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260221-1/f20b705defd0d791cd149b0b9a2248ce.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2886",
+        "vod_name": "JUFE-578-C オヤジのハメ撮りドキュメント ねっとり濃厚に貪り尽くす体液ドロドロ汗だく性交",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/4856f2540c8428361cc33001b3275932.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2877",
+        "vod_name": "JUFE-577-C 緊縛解禁！白い柔肌を縛り上げる令嬢緊縛",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/07078678b70cb37e3348e4cebef308ba.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2862",
+        "vod_name": "JUFE-575-C むっちり超乳Lcup人気フードルが欲しがる変態汁だく生セックス絶頂中毒",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/f40d5f5a1d6ac8c66a3ccd7452e9a883.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2861",
+        "vod_name": "JUFE-574-C 主観調教 俺だけの巨乳オナサポペット ドMの教え子に射精お手伝いを教え込む",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260602-1/4eb2898d2ebafd7728f09841cbadd739.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2827",
+        "vod_name": "MVSD-591-C 彼女のあどけない笑顔と魔性の誘惑に理性を失った私は…巧みな痴女テクといやらしい腰使いで中年",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/cf9443c4f7e4c92f4d6ec4103ac32a6a.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2826",
+        "vod_name": "MVSD-597-C 保健室のデカパイに一度でいいからあやかりたい！（切実） 111cm ！Mカップ！チ○ポドク",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/751c7adfc1b1c743252bf6aaf3036b4d.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2813",
+        "vod_name": "MVSD-593-C 異常肛門性愛おじさんにアナルをもてあそばれ開発されアナルSEXの悦びを覚えてしまった夏休み",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/ee5323722f3b45d4ffacfa37804615ff.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2812",
+        "vod_name": "MVSD-599-C ナースコールを押せば速攻チ〇ポをしゃぶりに来てくれる即尺フェラナース！ 彼女はボクのオチ〇",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260222-1/4aa1c68db6e26e7726763ee5d50c5dec.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2773",
+        "vod_name": "MUKD-499-C 悪徳チケット転売ヤーに騙され中出し取引きで何度も種付けされた無垢な田舎少女",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260220-1/ed1bc35638469fd9da6ae259eee2a1e6.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2772",
+        "vod_name": "MUKD-506-C オジサンの事を見下している生意気な少女達を理解らせWレ×プ ひまり すみれ",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260220-1/586ac814d704596d4fb66698965cca95.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2760",
+        "vod_name": "MUKD-496-C オジサンを見下す糞生意気なメスガキを徹底的に理解らせる。 若さを売りにするパ●活制服女子が",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260220-1/b2f4464f803c124936466923d6f37cc4.jpg",
+        "vod_remarks": ""
+      },
+      {
+        "vod_id": "2690",
+        "vod_name": "MRSS-157-C NTR介護 「ジジイの言うことを真に受けるな」と教えたのに、純粋な妻はつい優しく接してしま",
+        "vod_pic": "https://ewrzka4.naitang8.top/upload/vod/20260216-1/85fc9c42097f10ceda10e80c9c810080.jpg",
+        "vod_remarks": ""
+      }
+    ]
+  }
 }
